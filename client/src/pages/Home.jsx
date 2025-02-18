08840326fd3ea95b3f6ff3148d54ea89fa4b5f2c
AdemHos <ademhos1907@outlook.com>
2025-02-18 19:54:33 +0300

    Routes has created

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-600 font-bold">Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
