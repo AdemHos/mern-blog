aded21302c7df8cb2d92dff2ae45f4d371fee8a8
AdemHos <ademhos1907@outlook.com>
2025-02-18 22:19:55 +0300

    Header has completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import {BrowserRouter,Routes,Route } from "react-router-dom"
 import Home from './pages/Home'
+import Header from "./components/Header"
+
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path="/" element={<Home/>}/>
    </Routes>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-blue-600 font-bold">Home Page</h1>
+     
     </div>
   )
 }
