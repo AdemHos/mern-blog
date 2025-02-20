cda65f7602e8114985526d771663b00005179447
AdemHos <ademhos1907@outlook.com>
2025-02-20 16:04:49 +0300

    MongoDB  Connect

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+mongoose.connect(
+   process.env.MONGO
+)
+.then(() => {
+    console.log("Mongo DB has Connected")
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 
